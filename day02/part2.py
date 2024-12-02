d49798d25df930a13964eb8638608f25c0bd83c9
Amirul <hwdl2802@gmail.com>
2024-12-03 00:01:37 +0800

    day 2 finished at 00:01 am the next day :skull:

---
+++ day02/part2.py
@@ -0,0 +1,46 @@
+import os.path
+from typing import List
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def solve(s: str) -> int:
+    lines = s.splitlines()
+    for line in lines:
+        print(line)
+
+    # SOLUTION
+    # Determine if rows are safe, with a tolerance of one removed number from the sequence.
+    sequences = [list(map(int, line.split())) for line in lines]
+    return sum([check_tolerant_safety(seq) for seq in sequences])
+
+def check_safety(seq: List[int]) -> bool:
+    diffs = [seq[i+1] - seq[i] for i in range(len(seq) - 1)]
+    return (max(diffs) <= 3 and min(diffs) >= 1) or (max(diffs) <= -1 and min(diffs) >= -3)
+
+def check_tolerant_safety(seq: List[int]) -> bool:
+    return any(check_safety(seq[:i]+seq[i+1:]) for i in range(len(seq)))
+
+INPUT_S = '''\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+'''
+EXPECTED = 4
+
+
+def test() -> None:
+    assert solve(INPUT_S) == EXPECTED
+
+
+def main() -> int:
+    with open(INPUT_TXT) as f:
+        print(solve(f.read()))
+    return 0
+
+
+if __name__ == '__main__':
+    main()
