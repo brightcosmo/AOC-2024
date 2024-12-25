a7cfc5b63420a983ca087cf6e52e7e894fc40daf
Amirul Azizol <hwdl2802@gmail.com>
2024-12-25 20:04:29 +0800

    day 25: merry christmas

---
+++ day25/part1.py
@@ -0,0 +1,72 @@
+import os.path
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def solve(s: str) -> int:
+    lines = s.splitlines()
+    for line in lines:
+        print(line)
+    
+    # SOLUTION
+    # Return the number of lock/key pairs that fit together without overlap.
+    grids = s.strip().split("\n\n")
+    items = [{i for i, c in enumerate(item) if c == '#'} for item in grids]
+    return sum(not k & l for k in items for l in items) // 2
+
+INPUT_S = '''\
+#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####
+'''
+EXPECTED = 3
+
+
+def test() -> None:
+    assert solve(INPUT_S) == EXPECTED
+
+
+def main() -> int:
+    with open(INPUT_TXT) as f:
+        print(solve(f.read()))
+    return 0
+
+
+if __name__ == '__main__':
+    main()
